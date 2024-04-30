d383f831b6e8a66793eb6a8149b3f1df980004ef
Iqra Siddiqui <59932139+IqraSiddiqui@users.noreply.github.com>
2024-05-01 02:45:55 +0500

    Update App.js

--- w2_front/src/App.js
+++ w2_front/src/App.js
@@ -8,7 +8,7 @@ import UploadComponent from './UploadComponent';
 function App() {
 
   return (
-    <div style={{ background: 'linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 150, 0, 0.6))', height: '100vh', width: '500vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+    <div style={{ background: 'linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 150, 0, 0.6))', height: '100vh', width: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
     <UploadComponent/>
     <ChatComponent/>
     </div>
